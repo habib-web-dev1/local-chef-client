883cf2b5521ebd9cc5a9d6df48f6f6534535e8bc
MD AHSAN HABIB <MD AHSAN HABIB>
2025-12-21 04:59:41 +0600

    Set Mainlayout

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+import { ToastContainer } from "react-toastify"; /
+import Container from "../Components/Shared/Container";
+import Navbar from "../Components/Shared/Navbar";
+import Footer from "../Components/Shared/Footer";
+
+const MainLayout = () => {
+  return (
+    <Container>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Outlet />
+        <Footer />
+        <ToastContainer position="bottom-right" autoClose={3000} />
+      </div>
+    </Container>
+  );
+};
+
+export default MainLayout;
