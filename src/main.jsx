a8134728bd262edfa4c14c1acf6b35bff025dd35
MD AHSAN HABIB <MD AHSAN HABIB>
2025-12-21 03:49:01 +0600

    Complete folder setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./routes/Routes.jsx";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
+      </AuthProvider>
+    </HelmetProvider>
+  </StrictMode>
+);
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,134 @@
+import { createBrowserRouter } from "react-router";
+import ErrorPage from "../Pages/Shared/ErrorPage";
+import Home from "../Pages/Home";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
+import MealsPage from "../Pages/MealsPage";
+import MealDetails from "../Pages/MealDetails";
+import MyProfile from "../Pages/Dashboard/User/MyProfile";
+import MyOrders from "../Pages/Dashboard/User/MyOrders";
+import FavoriteMeals from "../Pages/Dashboard/User/FavoriteMeals";
+import MyReviews from "../Pages/Dashboard/User/MyReviews";
+import DashboardLayout from "../Pages/Dashboard/DashboardLayout";
+import MyMeals from "../Pages/Dashboard/Chef/MyMeals";
+import OrderRequests from "../Pages/Dashboard/Chef/OrderRequests";
+import PlatformStatistics from "../Pages/Dashboard/Admin/PlatformStatistics";
+import ManageUsers from "../Pages/Dashboard/Admin/ManageUsers";
+import ManageRequests from "../Pages/Dashboard/Admin/ManageRequests";
+import AdminRoute from "./AdminRoute";
+import ChefRoute from "./ChefRoute";
+import PrivateRoute from "./PrivateRoute";
+import UpdateMeal from "../Pages/Dashboard/Chef/UpdateMeal";
+import OrderPage from "../Pages/OrderPage";
+import DashboardHome from "../Pages/Dashboard/DashboardHome";
+import CreateMeal from "../Pages/Dashboard/Chef/CreateMeal";
+import PaymentSuccess from "../Pages/Shared/PaymentSuccess";
+import MainLayout from "../Layouts/MainLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/meals", element: <MealsPage /> },
+      {
+        path: "/meal/:id",
+        element: (
+          <PrivateRoute>
+            <MealDetails />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/order/:id",
+        element: (
+          <PrivateRoute>
+            <OrderPage />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      { index: true, element: <DashboardHome /> },
+
+      { path: "profile", element: <MyProfile /> },
+      { path: "my-orders", element: <MyOrders /> },
+      { path: "payment-success", element: <PaymentSuccess /> },
+      { path: "favorite-meals", element: <FavoriteMeals /> },
+      { path: "my-reviews", element: <MyReviews /> },
+
+      {
+        path: "create-meals",
+        element: (
+          <ChefRoute>
+            <CreateMeal />
+          </ChefRoute>
+        ),
+      },
+      {
+        path: "my-meals",
+        element: (
+          <ChefRoute>
+            <MyMeals />
+          </ChefRoute>
+        ),
+      },
+      {
+        path: "order-requests",
+        element: (
+          <ChefRoute>
+            <OrderRequests />
+          </ChefRoute>
+        ),
+      },
+      {
+        path: "update-meal/:id",
+        element: (
+          <ChefRoute>
+            <UpdateMeal />
+          </ChefRoute>
+        ),
+      },
+
+      {
+        path: "platform-stats",
+        element: (
+          <AdminRoute>
+            <PlatformStatistics />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-users",
+        element: (
+          <AdminRoute>
+            <ManageUsers />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-requests",
+        element: (
+          <AdminRoute>
+            <ManageRequests />
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+export default router;
