850a61d5c339a3a670229b43962980e59f665c25
MD AHSAN HABIB <MD AHSAN HABIB>
2025-12-21 11:25:44 +0600

    Complete private route page

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../Providers/AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    //
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+        <p className="ml-2">Loading user data...</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
+};
+
+export default PrivateRoute;
